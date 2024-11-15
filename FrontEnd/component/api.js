e8f3dfdc97429a0241b12d489a9b37291416b469
Annabelle Maufroy <annabelle.maufroy@gmail.com>
2024-11-15 11:10:38 +0100

    Update login

--- FrontEnd/component/api.js
+++ FrontEnd/component/api.js
@@ -1,28 +1,29 @@
-export async function fetchWorks(){
-    try{
-        const response = await fetch('http://localhost:5678/api/works');
-        return await response.json();
-    }catch (error){
-        console.error("Erreur récupération Works:", error);
+export async function fetchWorks() {
+    try {
+        const response = await fetch("http://localhost:5678/api/works")
+        return await response.json()
+    } catch (error) {
+        console.error("Erreur récupération Works:", error)
     }
 }
 
 export async function fetchCategories() {
-    try{
-        const response = await fetch('http://localhost:5678/api/categories')
-        return await response.json();
-    }catch (error){
-        console.error("Erreur récupération Catégories:", error);
+    try {
+        const response = await fetch("http://localhost:5678/api/categories")
+        return await response.json()
+    } catch (error) {
+        console.error("Erreur récupération Catégories:", error)
     }
 }
-export async function loginUser (formData){
-    try{
-        const response =  await fetch ('http://localhost:5678/api/users/login',{
-            method : 'POST',
-            body: formData
-        });
-        return await response.json();
-    }catch (error){
-        console.error('Erreur dans loginUser:', error);
+export async function loginUser(formData) {
+    try {
+        const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+        })
+        return await response.json()
+    } catch (error) {
+        console.error("Erreur dans loginUser:", error)
     }
 }
--- FrontEnd/component/login.js
+++ FrontEnd/component/login.js
@@ -1,46 +1,50 @@
-import { createForm } from "./form.js";
-import { loginUser } from "./api.js";
+import { createForm } from "./form.js"
+import { loginUser } from "./api.js"
+import { workPage } from "./projet.js"
 
-export async function loginPage(){
-    const main = document.querySelector('main');
-    const section = document.createElement('section');
-    section.classList.add('sectionForm');
-    main.innerHTML= '';
-    const form = document.createElement('form');
-    form.classList.add('formLogin');
+export async function loginPage() {
+    const main = document.querySelector("main")
+    const section = document.createElement("section")
+    section.classList.add("sectionForm")
+    main.innerHTML = ""
+    const form = document.createElement("form")
+    form.classList.add("formLogin")
 
-    const title = document.createElement('h2');
-    title.classList.add('titleLogin');
-    title.textContent='Log In';
-    section.appendChild(title);
+    const title = document.createElement("h2")
+    title.classList.add("titleLogin")
+    title.textContent = "Log In"
+    section.appendChild(title)
 
-    
-    createForm(form,'email','E-mail');
-    createForm(form,'password', 'Mot de passe');
+    createForm(form, "email", "E-mail")
+    createForm(form, "password", "Mot de passe")
 
-    const button = document.createElement('button');
-    button.classList.add('buttonConnect')
-    button.type ='submit';
-    button.textContent = 'Se connecter';
-    form.appendChild(button);
-    
-    section.appendChild(form);
+    const button = document.createElement("button")
+    button.classList.add("buttonConnect")
+    button.type = "submit"
+    button.textContent = "Se connecter"
+    form.appendChild(button)
 
-    const forgetLink = document.createElement('a')
-    forgetLink.classList.add('forgetLink');
-    forgetLink.textContent ='Mot de passe oublié'
-    section.appendChild(forgetLink);
+    section.appendChild(form)
 
-    main.appendChild(section);
+    const forgetLink = document.createElement("a")
+    forgetLink.classList.add("forgetLink")
+    forgetLink.textContent = "Mot de passe oublié"
+    section.appendChild(forgetLink)
 
-    form.addEventListener('submit', async (event)=>{
-        event.preventDefault();
-        const formData = new FormData(form);
-        try {
-            const userData = await loginUser(formData);
-            console.log('Connexion réussie', userData);
-        } catch (error) {
-            console.error('Erreur de connexion', error);
+    main.appendChild(section)
+
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault()
+        const dataForm = {
+            email: form.email.value,
+            password: form.password.value,
         }
-    });
+        try {
+            const userData = await loginUser(dataForm)
+            console.log("Connexion réussie", userData)
+            workPage();
+        } catch (error) {
+            console.error("Erreur de connexion", error)
+        }
+    })
 }
